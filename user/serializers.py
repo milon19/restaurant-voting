dfad69ab59e8f6d295092b623759046f0d5cb8c1
milon19 <milonmahato67@gmail.com>
2023-10-24 15:19:53 +0600

    create employee API.
    - create employee model and manager
    - create user along with employee
    - create API for create employee
    - write test cases

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
 from rest_framework import serializers
+
+from user.models import Employee
 
 User = get_user_model()
 
@@ -9,3 +12,20 @@ class AuthenticatedUserDetailSerializer(serializers.ModelSerializer):    # noqa
     class Meta:
         model = User
         fields = ['id', 'email', 'is_staff', 'is_superuser']
+
+
+class EmployeeCreateSerializer(serializers.Serializer):    # noqa
+    first_name = serializers.CharField(max_length=255, required=False)
+    last_name = serializers.CharField(max_length=255, required=False)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(max_length=255, write_only=True, required=True)
+
+    @staticmethod
+    def validate_email(value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email address already exists.")
+        return value
+
+    def save(self, **kwargs):
+        employee = Employee.objects.create_employee(**self.validated_data)
+        return employee
